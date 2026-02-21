62e19a960403559082cc7c2a65bfd126c0a700c2
Periyasamy Kaliyan <samy8144@gmail.com>
2026-02-21 20:55:52 +0530

    Add files via upload

---
+++ Assessment-1 control structure.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+print("Welcome to Assignment-1 ")
+
+
+# In[4]:
+
+
+Num1= 10
+Num2= 30
+Add=Num1+Num2
+print(Add)
+
+
+# In[5]:
+
+
+#Body Mass Index
+
+
+# In[25]:
+
+
+BMI=float(input("Enter your index value : "))
+
+if(BMI<=18):
+        print("Underweight")
+elif(BMI<=18.5):
+        print("Thin for height")
+elif(BMI==18.6):
+        print("Healthy weight")
+elif(BMI<=24.9):
+        print("Healthy weight")
+elif(BMI==25):
+        print("Overweight")
+elif(BMI<=29.9):
+        print("Overweight")
+else:
+        print("Obesity")
+
+
+# In[ ]:
+
+
+
+
